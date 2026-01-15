bd5cadb7564e5f7bc0ab83f203996a9a41969a77
Felipe Becerra <felipe.e.becerra@gmail.com>
2026-01-15 17:01:25 -0300

    server: added soft delete metho

--- server/controllers/note.controller.js
+++ server/controllers/note.controller.js
@@ -11,14 +11,25 @@ const findAll = async (req, res, next) => {
 
 const upsert = async (req, res, next) => {
     try {
-        const note = await noteService.upsertNote(req.body)
-        res.status(201).json(note)
+        const newNote = await noteService.upsertNote(req.body)
+        res.status(201).json(newNote)
     } catch (error) {
         next(error)
     }
 }
 
+const softDelete = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const deletedNote = await noteService.softDeleteNote(id)
+        res.status(200).json({message: "Note successfully deleted"});
+    } catch (error) {
+        next(error);
+    }
+}
+
 export default {
     findAll,
-    upsert
+    upsert,
+    softDelete
 }
